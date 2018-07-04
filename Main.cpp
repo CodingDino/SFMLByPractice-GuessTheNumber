7e3a7c04a6d57df047d31b3979d9e434d19ebd70
Sarah Herzog <sarah.ann.herzog@gmail.com>
2018-07-04 19:11:52 +0100

    Number input from player

--- Main.cpp
+++ Main.cpp
@@ -44,6 +44,61 @@ int main()
 	std::cout << std::endl;
 
 	// -----------------------------------------------
+	// Guessing section
+	// -----------------------------------------------
+
+	// Create a variable to hold the player guess
+	// Assign a value of 0 to the variable by default
+	int guess = 0;
+
+	// Print out a prompt to the player to tell them to guess
+	std::cout << "Please guess a number between 1 and 10: " << std::endl;
+	std::cout << "> ";
+
+	// Get input from the player and store it in the guess variable
+	std::cin >> guess;
+
+	// Print out the player's guess
+	std::cout << std::endl;
+	std::cout << "You guessed: " << guess << std::endl;
+	std::cout << std::endl;
+
+	// Check whether the player's guess was correct
+	if (guess == secretNumber)
+	{
+		// This code will be executed if the guess is correct
+
+		// Print feedback to the player
+		std::cout << "Your guess is correct" << std::endl;
+		std::cout << std::endl;
+	} // end of correct guess if body
+	else
+	{
+		// This code will be executed if the guess is wrong
+
+		// Print feedback to the player
+		std::cout << "Your guess is wrong" << std::endl;
+
+		// Check whether the player's guess was too low or too high
+		if (guess < secretNumber)
+		{
+			// This code will be executed if the guess is too low 
+
+			// Print feedback to the player
+			std::cout << "Your guess is too low" << std::endl;
+			std::cout << std::endl;
+		} // end of low/high if body
+		else
+		{
+			// This code will be executed if the guess is too high
+
+			// Print feedback to the player
+			std::cout << "Your guess is too high" << std::endl;
+			std::cout << std::endl;
+		} // end of low/high else body
+	} // end of correct guess else body
+
+	// -----------------------------------------------
 	// End the program
 	// -----------------------------------------------
 
